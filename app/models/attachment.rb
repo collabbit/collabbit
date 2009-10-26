922554e38d027967576fb053dd3cd36227ff2d27
Sam DeFabbia-Kane <sdefabbiakan@wesleyan.edu>
2009-10-26 20:22:55 +0000

    attachments now appear on the updates page; attachment links fixed

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -3,7 +3,7 @@ class Attachment < ActiveRecord::Base
   
   has_attached_file :attach,
     :path => ":rails_root/attachments/:instance_id/:id/:basename.:extension",
-    :url => "/instances/:instance_id/incidents/:incident_id/updates/:update_id/attachments/:id"
+    :url => "/for/:instance_id/incidents/:incident_id/updates/:update_id/attachments/:id"
   validates_attachment_size :attach, :less_than => 5.megabytes
   attr_protected :attach_file_name, :attach_content_type, :attach_file_size, :attach_updated_at
 
