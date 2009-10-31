db45c4df4f77034e268a24d62a79a815e60c7d58
Eli Fox-Epstein <eli@elilies.com>
2009-10-31 03:50:28 +0000

    email settings are golden

---
+++ config/initializers/smtp_settings.sample.rb
@@ -0,0 +1,9 @@
+# ActionMailer::Base.smtp_settings = {
+#     :enable_starttls_auto => true,
+#     :address        => 'smtp.gmail.com',
+#     :port           => 587,
+#     :domain         => 'collabbit.org',
+#     :authentication => :plain,
+#     :user_name      => 'notifications@collabbit.org',
+#     :password       => 'password'
+# }
