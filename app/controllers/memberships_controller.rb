4c1e4ee81576d5a08450ea967c1f89229f2fc8be
Sam DeFabbia-Kane <sdefabbiakan@wesleyan.edu>
2009-11-01 03:42:41 +0000

    the 'join group' link only shows up in the group list if you're not already in the group. individual join group pages now show a join group link if you're not in the group, and a leave group link if you are

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -6,10 +6,17 @@ class MembershipsController < ApplicationController
     
     return with_rejection unless @user.updatable?
     
-    @user.groups << @group unless @user.groups.include?(@group)
+    if params[:leave]
+      @user.groups.delete @group
+
+      flash[:notice] = "You have left this group."
+      redirect_to instance_group_type_group_path(@instance, @group.group_type, @group)
+    else
+      @user.groups << @group unless @user.groups.include?(@group)
     
-    flash[:notice] = "You have joined this group."
-    redirect_to instance_group_type_group_path(@instance, @group.group_type, @group)
+      flash[:notice] = "You have joined this group."
+      redirect_to instance_group_type_group_path(@instance, @group.group_type, @group)
+    end
   end
   def delete
     
