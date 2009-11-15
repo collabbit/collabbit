629922c4fbf58320f23fbb6525df90913285163e
Eli Fox-Epstein <eli@elilies.com>
2009-11-15 02:30:36 +0000

    adding membership count and email alert

---
+++ db/migrate/20091114154341_add_memberships_count.rb
@@ -0,0 +1,15 @@
+class AddMembershipsCount < ActiveRecord::Migration
+  def self.up
+    add_column :groups, :memberships_count, :integer, :default => 0
+
+    Group.reset_column_information
+    Group.all.each do |g|
+      Group.update_counters g.id, :memberships_count => g.memberships.count
+    end
+  end
+
+  def self.down
+    remove_column :groups, :memberships_count
+  end
+end
+
