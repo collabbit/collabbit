af8e030e0d0d2f0648de96f49ec02710ad819dbb
Eli Fox-Epstein <eli@elilies.com>
2009-12-06 20:59:44 +0000

    sorry, dimitar

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,5 +17,3 @@ config.action_controller.perform_caching             = false
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.perform_deliveries = true
 #config.action_mailer.delivery_method = :sendmail
-
-config.middleware.use "Rack::Bug"
