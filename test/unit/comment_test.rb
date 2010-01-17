5a0289e7ae7e3319fde33c490991e6ab5c50603b
Eli <eli@elilies.com>
2010-01-17 12:18:37 -0500

    Limit comment min and max lengths

--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  fixtures :all
+  
+  should_belong_to :user
+  should_belong_to :update
+  
+  should_ensure_length_in_range :body, 2..4096
+  should_validate_presence_of :body
 end
