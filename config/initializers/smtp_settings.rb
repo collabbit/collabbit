55d846a1bdd570af0b7b74fa3d0210c94b237062
Eli <eli@elilies.com>
2010-01-20 19:50:39 -0500

    Move mail settings to YAML

---
+++ config/initializers/smtp_settings.rb
@@ -0,0 +1,12 @@
+settings = YAML.load_file("config/smtp.yml")[ENV['RAILS_ENV']]
+
+ActionMailer::Base.smtp_settings = {
+  :enable_starttls_auto => settings['tls'],
+  :address        => settings['address'],
+  :port           => settings['port'] || 25,
+  :domain         => settings['domain'],
+  :authentication => (settings['authentication'] && settings['authentication'].to_sym) || :plain,
+  :user_name      => settings['username'],
+  :password       => settings['password']
+}
+
--- config/initializers/smtp_settings.sample.rb
+++
@@ -1,9 +0,0 @@
-# ActionMailer::Base.smtp_settings = {
-#     :enable_starttls_auto => true,
-#     :address        => 'smtp.gmail.com',
-#     :port           => 587,
-#     :domain         => 'collabbit.org',
-#     :authentication => :plain,
-#     :user_name      => 'notifications@collabbit.org',
-#     :password       => 'password'
-# }
