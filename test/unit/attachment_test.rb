d1e4c8bf9ad1cc4325c94634a9af4dc574b622d0
Eli <eli@elilies.com>
2010-01-28 11:45:27 -0500

    Add attachment unit test

--- test/unit/attachment_test.rb
+++ test/unit/attachment_test.rb
@@ -1,8 +1,6 @@
 require 'test_helper'
 
 class AttachmentTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  should_not_allow_mass_assignment_of :attach_file_name, :attach_content_type, :attach_file_size, :attach_updated_at
+  should_belong_to :update
 end
