48eb0353fb47160b10d2546df190b7fa1013f5f4
Eli <eli@elilies.com>
2010-02-11 23:24:02 -0500

    Add unit tests

--- test/unit/classification_test.rb
+++ test/unit/classification_test.rb
@@ -1,8 +1,6 @@
 require 'test_helper'
 
 class ClassificationTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  should_belong_to :update
+  should_belong_to :group
 end
