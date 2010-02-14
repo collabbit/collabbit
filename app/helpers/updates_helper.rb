242b3c94a4e416caa278c832afb2cbb65ef63f80
Sam DeFabbia-Kane <sdefabbiakane@munin.localdomain>
2010-02-14 00:25:48 -0500

    Cleaned up the truncation being done for the update summaries on the 'incident update' page. Truncation now occurs at the end of the last full word, rather than exactly at the 256th character.

--- app/helpers/updates_helper.rb
+++ app/helpers/updates_helper.rb
@@ -7,5 +7,18 @@ module UpdatesHelper
       link_to u.issuing_group.name, group_type_group_url(u.issuing_group.group_type, u.issuing_group)
     end
   end
+  
+  # trims at the last word ending at or before 'len'
+  def truncate_at(str,len)
+    truncated = str[0..len+1]
+    if truncated[-1] =~ /\s/
+      truncated.strip!
+    else
+      words = truncated.split(' ')
+      words.delete_at(-1)
+      truncated = words.join(' ')
+    end
+    truncated
+  end
 
 end
