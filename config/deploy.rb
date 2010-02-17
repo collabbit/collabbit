83654a0be8dd071dcd11bf9cb1fd03bf50dea3a0
Eli <eli@elilies.com>
2010-02-17 11:41:15 -0500

    Update deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-require 'erb'
+# gem required: sudo gem install capistrano-ext
 
 def load_settings(env)
   set_settings(YAML.load_file("config/settings/deploy.yml")[env.to_s])
@@ -7,6 +7,7 @@ end
 def set_settings(params)
   params.each_pair do |k,v|
     set k.to_sym, v
+    puts "Setting #{k} to #{v}"
   end
   if exists? :domain
     role :app, domain
@@ -15,11 +16,52 @@ def set_settings(params)
   end
 end
 
-# gem required: sudo gem install capistrano-ext
+def configuration_for(name, settings = {})
+  settings[:path], settings[:filename] = File.split(settings[:filepath]) if settings[:filepath]
+  settings[:path] ||= "config/settings"
+  settings[:chmod] = 775 unless settings.key? :chmod
+  
+  after 'deploy:update_code', "#{name}:symlink"
+  before 'deploy:setup', name.to_sym
+  namespace(name.to_sym) do    
+    desc "Configuration for #{name}"
+    task(:default) do
+      set(:shared_dir) { File.join(fetch(:shared_path), settings[:path]) }
+      set(:shared_file) { File.join(fetch(:shared_dir), settings[:filename]) } if settings[:filename]
+      run "mkdir -p #{shared_dir}"
+      run "chmod #{settings[:chmod]} #{shared_dir}" if settings[:chmod]
+      if settings[:filename]
+        settings[:text] = yield if block_given?
+        puts settings[:text], shared_file
+        run "chmod #{settings[:chmod]} #{shared_file}"
+      elsif block_given?
+        yield
+      end
+    end
+    # Same as foo.gsub /\/\z/, '', probably
+    sym_path = File.join(File.split("#{settings[:path]}/#{settings[:filename]}"))
+    symlink_task name, sym_path, :chmod => settings[:chmod]
+  end
+end
+
+def symlink_task(nspace, path, settings = {})
+  settings[:chmod] = 775 unless settings.key? :chmod
+  desc "Create the symlink for #{File.basename(path)}"
+  namespace(nspace.to_sym) do
+    task(:symlink) do
+      from = File.join(fetch(:shared_path), path)
+      to = File.join(fetch(:release_path), path)
+      run "mkdir -p #{File.dirname(to)}"
+      run "chmod #{settings[:chmod]} #{File.dirname(to)}" if settings[:chmod]
+      run "ln -nfs #{from} #{to}"
+    end
+  end
+end
+
 set :stages, %w(staging production)
 require 'capistrano/ext/multistage'
-
 set :application, 'collabbit'
+
 set :use_sudo,    false
 set :scm,         :git
 set :deploy_via,  :remote_cache
@@ -29,32 +71,21 @@ set :deploy_via,  :remote_cache
 ssh_options[:paranoid] = false
 default_run_options[:pty] = true
 
-after "deploy:update_code", 'db:symlink', 'mail:symlink', 'attachments:symlink', 'exceptional:symlink', :gems
-before "deploy:setup", :db, :mail, :attachments, :exceptional
+before 'deploy:restart', :gems
   
 namespace :passenger do
 
-  desc <<-DESC
-    Restarts your application. \
-    This works by creating an empty `restart.txt` file in the `tmp` folder
-    as requested by Passenger server.
-  DESC
+  desc "Restarts your application server."
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{current_path}/tmp/restart.txt"
   end
   
-  desc <<-DESC
-    Starts the application servers. \
-    Please note that this task is not supported by Passenger server.
-  DESC
+  desc "Starts the application servers." 
   task :start, :roles => :app do
     logger.info ":start task not supported by Passenger server"
   end
   
-  desc <<-DESC
-    Stops the application servers. \
-    Please note that this task is not supported by Passenger server.
-  DESC
+  desc "Stops the application servers."
   task :stop, :roles => :app do
     logger.info ":stop task not supported by Passenger server"
   end
@@ -89,103 +120,63 @@ namespace :deploy do
   end
 end
 
-namespace :db do
-  desc "Create database yaml in shared path" 
-  task :default do
-    set :db_user do
-      Capistrano::CLI.ui.ask 'Database Username: '
-    end
-    set :db_pass do
-     Capistrano::CLI.password_prompt 'Database Password: '
-    end
-    db_config = ERB.new <<-EOF
-    production:
-      database: #{production_database}
-      adapter: mysql
-      encoding: utf8
-      username: #{db_user}
-      password: #{db_pass}
-      
-    development:
-      database: #{development_database}
-      adapter: mysql
-      encoding: utf8
-      username: #{db_user}
-      password: #{db_pass}
-    EOF
 
-    run "mkdir -p #{shared_path}/config" 
-    put db_config.result, "#{shared_path}/config/database.yml" 
-  end
+configuration_for :db, :filepath => 'config/database.yml' do
+  set(:db_user) { Capistrano::CLI.ui.ask 'Database Username: ' }
+  set(:db_pass) { Capistrano::CLI.password_prompt 'Database Password: ' }
 
-  desc "Make symlink for database yaml" 
-  task :symlink do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
-  end
+  <<-EOF
+  production:
+    database: #{production_database}
+    adapter: mysql
+    encoding: utf8
+    username: #{db_user}
+    password: #{db_pass}
+    
+  development:
+    database: #{development_database}
+    adapter: mysql
+    encoding: utf8
+    username: #{db_user}
+    password: #{db_pass}
+  EOF
 end
 
-namespace :exceptional do
-  desc "Create Exceptional"
-  task :default do
-    set :api_key do
-      Capistrano::CLI.ui.ask 'Exceptional API key?'
-    end
-    
-    exceptional_data = {
-      'production' => {
-        'api-key' => api_key,
-        'enabled' => true
+configuration_for :exceptional, :filepath => 'config/exceptional.yml' do
+  set :api_key do
+    Capistrano::CLI.ui.ask 'Exceptional API key?'
+  end
+  
+  exceptional_data = {
+    'production' => {
+      'api-key' => api_key,
+      'enabled' => true
+    }
+  }.to_yaml
+end
+
+configuration_for :mail, :filepath => 'config/settings/smtp.yml' do
+  {'production' => {
+      'port'     => 25,
+      'domain'   => 'collabbit.org',
+      'address'  => 'localhost',
+      'tls'      => false,
+      'authentication' => false
+    }
+  }.to_yaml
+end
+
+configuration_for :keys, :filepath => 'config/settings/keys.yml' do
+  {'action_controller' => {
+      'session' => {
+        'session_key' => '_collabbit-001-staging',
+        'secret' => '5695f525eb5ec94353765d4c12c64df56cadc68bb8ba49a1bb1967379bccfd994176645f677d7568cc114bf2f94b'
       }
     }
-    
-    run "mkdir -p #{shared_path}/config" 
-    put exceptional_data.to_yaml, "#{shared_path}/config/exceptional.yml"
-  end
-  
-  task :symlink do
-    run "ln -nfs #{shared_path}/config/exceptional.yml #{release_path}/config/exceptional.yml" 
-  end
-  
+  }.to_yaml
 end
 
-namespace :mail do
-  desc "Create mailserver yaml in shared path" 
-  task :default do    
-    smtp_settings = {
-      'production' => {
-        'port'     => 25,
-        'domain'   => 'collabbit.org',
-        'address'  => 'localhost',
-        'tls'      => false,
-        'authentication' => false
-      }
-    }
-
-    run "mkdir -p #{shared_path}/config" 
-    run "chmod 775 #{shared_path}/config"
-  
-    put smtp_settings.to_yaml, "#{shared_path}/config/smtp.yml"
-    run "chmod 775 #{shared_path}/config/smtp.yml"
-  end
-
-  desc "Make symlink for database yaml" 
-  task :symlink do
-    run "ln -nfs #{shared_path}/config/smtp.yml #{release_path}/config/smtp.yml" 
-  end
-end
-
-namespace :attachments do
-  desc "Make shared attachments directory"
-  task :default do
-    run "mkdir -p #{shared_path}/attachments"
-    run "chmod 775 #{shared_path}/attachments"
-  end
-  
-  desc "Link shared attachments directory"
-  task :symlink do
-    run "ln -nfs #{shared_path}/attachments #{release_path}/attachments"
-  end
-end
+configuration_for :attachments, :path => 'attachments'
 
 namespace :gems do
   desc "Update gems"
