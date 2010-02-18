df075c6fa25ca0d6256c3a9eeb12714f0c81288a
Eli <eli@elilies.com>
2010-02-17 20:46:27 -0500

    Update comment styling and dynamics

--- public/javascripts/updates.js
+++ public/javascripts/updates.js
@@ -1,55 +1,68 @@
-jQuery(function(){
+$.noConflict();
+jQuery(document).ready(function($){
     
-    var commentInitial = 'Add a comment here'
-    jQuery('.comment-textfield').each(function(){
-        jQuery(this).focus(function(){
-           	if(jQuery(this).val() == commentInitial)
-    			jQuery(this).val('').removeClass('blank'); 
-        }).blur(function(){
-            if(jQuery(this).val() == '')
-        		jQuery(this).val(commentInitial).addClass('blank');
-        });
-        jQuery(this).blur();
+    var commentInitial = 'Add a comment here';
+    
+    function focusComment($frm){
+        $txt = $frm.find('.comment-textfield');
+        if($txt.val() != commentInitial) return;
+        $txt.val('').removeClass('blank');
+        $frm.find('.comment-submit').show();
+    }
+    
+    function unfocusComment($frm){
+        $txt = $frm.find('.comment-textfield');
+        if($txt.val() != '') return;
+        $txt.val(commentInitial).addClass('blank');
+        $frm.find('.comment-submit').hide();
+    }
+    
+    $('.new_comment').each(function(){
+        var $frm = $(this);
+        $(this).find('.comment-textfield').focus(function(){  focusComment($frm) })
+                                          .blur(function(){ unfocusComment($frm) });
+               
+        unfocusComment($(this));
     });
 	
 	// for filters
 	var init = 'Search Keywords';
 
-	if (jQuery('#search').val() == '') {
-		jQuery('#search').val(init);
+	if ($('#search').val() == '') {
+		$('#search').val(init);
 	}
 	
-	jQuery('#search').focus(function(){
-		if(jQuery(this).val() == init)
-			jQuery(this).val('').removeClass('blank');
+	$('#search').focus(function(){
+		if($(this).val() == init)
+			$(this).val('').removeClass('blank');
 	}).blur(function(){
-		if(jQuery(this).val() == '')
-			jQuery(this).val(init).addClass('blank');
+		if($(this).val() == '')
+			$(this).val(init).addClass('blank');
 	});
 	
-	jQuery('#filters form').submit(function(){
+	$('#filters form').submit(function(){
 		if($('#search').val() == init)
 			$('#search').val('')
 		return true;
 	});
 
-	jQuery('#showallgroups').live("click",function(){
-		jQuery('.allgroups').removeClass('hide');
-		jQuery('#showallgroups').addClass('selected');
-		jQuery('#showmygroups').removeClass('selected');
+	$('#showallgroups').live("click",function(){
+		$('.allgroups').removeClass('hide');
+		$('#showallgroups').addClass('selected');
+		$('#showmygroups').removeClass('selected');
 		return false;
 	});
 
-	jQuery('#showmygroups').live("click",function(){
-		jQuery('.allgroups').addClass('hide');
-		jQuery('.allgroups input:checkbox').attr('checked',false);
-		jQuery('#showmygroups').addClass('selected');
-		jQuery('#showallgroups').removeClass('selected');
+	$('#showmygroups').live("click",function(){
+		$('.allgroups').addClass('hide');
+		$('.allgroups input:checkbox').attr('checked',false);
+		$('#showmygroups').addClass('selected');
+		$('#showallgroups').removeClass('selected');
 		return false;
 	});
 
 	// for file uploading
-	jQuery('#multi').MultiFile({
+	$('#multi').MultiFile({
 		
 	});
 });
