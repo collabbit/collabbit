a3e6c3da63560ceb96077e6e746068f1584fbf1b
Eli <eli@elilies.com>
2010-03-30 13:26:39 -0400

    Add authorization to attached files

--- app/controllers/attached_files_controller.rb
+++ app/controllers/attached_files_controller.rb
@@ -1,8 +1,11 @@
-class AttachedFilesController < ApplicationController
+class AttachedFilesController < AuthorizedController
   
   def show
     incident = @instance.incidents.find(params[:incident_id])
     update = incident.updates.find(params[:update_id])
+    
+    return with_rejection unless @current_user.can? :view => update
+    
     attachment = update.attached_files.find(params[:id])
     send_file(attachment.attach.path, :filename => attachment.attach.original_filename)
   end
