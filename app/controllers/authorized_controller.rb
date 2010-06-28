bb1b1743ccc784849d0ecc4a6aaaeff4a03cf4b0
Sam DeFabbia-Kane <sam@defabbiakane.com>
2010-06-28 11:45:31 -0400

    Removed some more copyright declarations.
    Pretty sure that was the last of them.

--- app/controllers/authorized_controller.rb
+++ app/controllers/authorized_controller.rb
@@ -1,8 +1,3 @@
-# Author::      Eli Fox-Epstein, efoxepstein@wesleyan.edu
-# Author::      Dimitar Gochev, dimitar.gochev@trincoll.edu
-# Copyright::   Humanitarian FOSS Project (http://www.hfoss.org), Copyright (C) 2009.
-# License::     http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
-
 class AuthorizedController < ApplicationController
   
   before_filter :check_right_account
--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,9 +1,3 @@
-# Represents an action that a user might need authorization to access 
-#
-# Author::      Eli Fox-Epstein, efoxepstein@wesleyan.edu
-# Author::      Dimitar Gochev, dimitar.gochev@trincoll.edu
-# Copyright::   Humanitarian FOSS Project (http://www.hfoss.org), Copyright (C) 2009.
-# License::     http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
 class Permission < ActiveRecord::Base
   include Authority
   
