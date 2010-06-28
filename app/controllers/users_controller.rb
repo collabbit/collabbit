71c54cf39de301422ec0d8963656c2cd92994fbe
Sam DeFabbia-Kane <sam@defabbiakane.com>
2010-06-28 11:43:17 -0400

    Removed some copyright declarations.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,3 @@
-# Controller for operations on users in the database.
-#
-# Author::      Eli Fox-Epstein, efoxepstein@wesleyan.edu
-# Author::      Dimitar Gochev, dimitar.gochev@trincoll.edu
-# Copyright::   Humanitarian FOSS Project (http://www.hfoss.org), Copyright (C) 2009.
-# License::     http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
-
 class UsersController < AuthorizedController
 
   skip_before_filter :require_login, :only => [:new, :create, :forgot_password,
