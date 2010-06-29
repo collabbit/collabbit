1caf49ccea19d8445e080ad35a86ce45187e7be7
Sam DeFabbia-Kane <sam@defabbiakane.com>
2010-06-29 10:34:11 -0400

    Removed copyright notice.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,9 +1,3 @@
-# Prepares welcome and activation messages to newly-registered users
-#
-# Author::      Eli Fox-Epstein, efoxepstein@wesleyan.edu
-# Author::      Dimitar Gochev, dimitar.gochev@trincoll.edu
-# Copyright::   Humanitarian FOSS Project (http://www.hfoss.org), Copyright (C) 2009.
-# License::     http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL)
 class UserMailer < ActionMailer::Base
   
   helper :application
