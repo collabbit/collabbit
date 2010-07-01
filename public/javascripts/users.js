b2f6d3d9a3d993429e7385045ab69bdf7504fba6
Sam DeFabbia-Kane <sam@defabbiakane.com>
2010-07-01 14:35:35 -0400

    User forms now have full support for alerts.

--- public/javascripts/users.js
+++ public/javascripts/users.js
@@ -18,4 +18,35 @@ $(function(){
 			$('#search').val('')
 		return true;
 	});
+
+	// for alert menu
+	var upArrow = '&#9650;'
+	var downArrow = '&#9660;'
+
+	$('#alerts-toggle').live("click",function(){
+		if ($('#alerts-individual').hasClass('hidden')) {
+			$('#alerts-individual').removeClass('hidden');
+			$('#alerts-toggle .arrow').html(upArrow);
+		} else {
+			$('#alerts-individual').addClass('hidden');
+			$('#alerts-toggle .arrow').html(downArrow);
+		}
+		return false;
+	});
+
+	// enable/disable single feed checkboxes based on master settings
+	$('#user_text_alert').live("click", function() {
+		if (this.checked) {
+			$('.feed-text-setting input').removeAttr('disabled');
+		} else {
+			$('.feed-text-setting input').attr('disabled','disabled');
+		}
+	});
+	$('#user_email_alert').live("click", function() {
+		if (this.checked) {
+			$('.feed-email-setting input').removeAttr('disabled');
+		} else {
+			$('.feed-email-setting input').attr('disabled','disabled');
+		}
+	});
 });
